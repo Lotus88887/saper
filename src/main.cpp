c507dbbd6e1ecb5f1135176ee5c2cc30581eba9e
Kacper Kaluza <97208353+kacperkaluza@users.noreply.github.com>
2025-05-12 20:16:13 +0200

    Add wxWidgets project and initial application setup
    Created a new Visual Studio project for a wxWidgets application. Added a `.gitignore` file to exclude build directories and Visual Studio files. Implemented a simple wxWidgets application in `main.cpp` that displays "Hello wxWidgets". Updated solution file (`src.sln`), project file (`src.vcxproj`), project filters (`src.vcxproj.filters`), and user settings (`src.vcxproj.user`) to support x64 and Win32 configurations.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit() override;
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame() : wxFrame(nullptr, wxID_ANY, "Hello wxWidgets") {}
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+    MyFrame* frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
