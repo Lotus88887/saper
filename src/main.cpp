52c9bd4353c42fa124202b3ed863bacd240ab17e
Kacper Kaluza <97208353+kacperkaluza@users.noreply.github.com>
2025-05-13 15:55:36 +0200

    Refactor application and frame class names for clarity
    Renamed `MyApp` to `MainApp` and `MyFrame` to `MainFrame` to improve naming consistency. Updated the `OnInit` method to instantiate `MainFrame`. Added a constructor for `MainFrame` that sets up a 9x9 grid of buttons with event handlers for interactions. Introduced new private methods for handling close events and button clicks, along with an event table for proper cleanup. Included `<vector>` for better management of dynamically created buttons.

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,80 @@
 #include <wx/wx.h>
+#include <vector>
 
-class MyApp : public wxApp
+class MainApp : public wxApp
 {
 public:
-    virtual bool OnInit() override;
+    virtual bool OnInit();
 };
 
-class MyFrame : public wxFrame
+
+
+wxIMPLEMENT_APP(MainApp);
+
+class MainFrame : public wxFrame
 {
 public:
-    MyFrame() : wxFrame(nullptr, wxID_ANY, "Hello wxWidgets") {}
+    MainFrame();
+private:
+    void OnClose(wxCloseEvent& event);
+    void OnButtonClicked(wxCommandEvent& event);
+    void OnButtonRightClick(wxMouseEvent& event);
+
+    std::vector<wxButton*> buttons;
+
+wxDECLARE_EVENT_TABLE();
+    
 };
 
-wxIMPLEMENT_APP(MyApp);
+wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
+EVT_CLOSE(MainFrame::OnClose)
+wxEND_EVENT_TABLE()
 
-bool MyApp::OnInit()
-{
-    MyFrame* frame = new MyFrame();
+bool MainApp::OnInit() {
+    MainFrame* frame = new MainFrame();
     frame->Show(true);
+    
     return true;
 }
+
+MainFrame::MainFrame() : wxFrame(NULL, wxID_ANY, "Minesweeper")
+{
+    Centre();
+    
+    wxGridSizer* gridSizer = new wxGridSizer(9, 9, 2, 2);
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            wxButton* myButton = new wxButton(this, wxID_ANY, "", wxDefaultPosition, wxSize(30, 30));
+            myButton->Bind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
+            myButton->Bind(wxEVT_RIGHT_UP, &MainFrame::OnButtonRightClick, this);
+            gridSizer->Add(myButton, 0, wxEXPAND);
+        }
+    }
+
+    this->SetSizerAndFit(gridSizer);
+}
+
+void MainFrame::OnClose(wxCloseEvent& event) {
+    if (event.CanVeto()) {
+        if (wxMessageBox("Do you want to exit?", "Please confirm", wxICON_QUESTION | wxYES_NO) != wxYES) {
+            event.Veto();
+            return;
+        }
+    }
+    event.Skip();
+}
+
+void MainFrame::OnButtonClicked(wxCommandEvent& event) {
+    wxMessageBox("Button was clicked!");
+}
+
+void MainFrame::OnButtonRightClick(wxMouseEvent& event) {
+    wxButton* btn = dynamic_cast<wxButton*>(event.GetEventObject());
+    if (btn) {
+        wxMessageBox("Right button was clicked!");
+        // You can set a flag, change label, etc. here
+    }
+}
+
+
